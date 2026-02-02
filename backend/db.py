b3795a4ddc0e1b0bf55117b6cc15169afb92e9ad
ruo-edwin <ruoedwin@gmail.com>
2026-02-02 13:09:44 +0300

    alembic

--- backend/db.py
+++ backend/db.py
@@ -9,7 +9,7 @@ from pathlib import Path
 env_path = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path=env_path)   
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("DEBUG: DATABASE_URL =", DATABASE_URL)
+
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is missing or not set.")
